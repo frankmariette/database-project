2925def0fb1e462f9d47c3ce3aae0211347142a4
Nate Thompson <nthompson55@gmail.com>
2014-04-14 23:05:47 -0500

    Group work

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ var passport = require('passport');
 
 // Database connection. Modify conString for your own local copy
 var pg = require('pg');
-var conString = "postgres://karrde00@localhost/karrde00";
+var conString = "postgres://nate:51147C0le@localhost:5432/nate";
 
-/*
+
 pg.connect(conString, function(err, client, done){
 	if(err){
 		return console.error('error fetching client from pool', err);
@@ -31,7 +31,7 @@ pg.connect(conString, function(err, client, done){
 		//output: 1
 	});
 });
-*/
+
 
 var app = express();
 
@@ -95,10 +95,22 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+//Working on sending data to state page checkout state.jade
 app.get('/state/:state',function(req, res) {
-  
-  res.render('state', {state_name: req.params.state});
+  state_code = state_name_to_code[req.params.state];
+  pg.connect(conString, function(err, client, done){
+    var query = client.query('SELECT * FROM political_data.congressmen JOIN political_data.house_term USING (gov_track_id) WHERE state_code = $1 AND session = $2',[state_code,113]);
+    query.on('row', function(row,result){
+    	result.addRow(row);
+    });
+    query.on("end", function (result) {
+      res.render('state', {state_name: req.params.state, state_code: state_code, reps: result});
+    client.end();
+    });
+  });     
 })
+
+
 app.get('/tables', function(req, res) {
   res.render('tables');
 });
@@ -131,3 +143,54 @@ app.get('/tables', function(req, res) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+state_name_to_code={'Alabama': 'AL',
+    'Alaska':'AK',
+    'Arizona':'AZ',
+    'Arkansas':'AR',
+    'California':'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE' ,
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois':'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA' ,
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan':'MI',
+    'Minnesota':'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana':'MT',
+    'Nebraska':'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee':'TN',
+    'Texas': 'TN',
+    'Utah': 'UT',
+    'Vermont':'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia':'WV',
+    'Wisconsin':'WI',
+    'Wyoming': 'WY'};
